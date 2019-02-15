fe6a9d56fcae8a759559bbc8c2683ca93b03b0f0
Alexis <alexis.morin.mail@gmail.com>
2019-02-15 01:41:13 -0500

    added menu template and wiimote demo scene

---
+++ JN2018_04/Assets/Scripts/WiimoteObjectMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WiimoteObjectMover : MonoBehaviour {
+
+    public WiimoteTest wiimoteMotionReceiver;
+    Vector3 gameMotion;
+    public float moveSpeed = 1f;
+    public Transform target;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
+    void LateUpdate () {
+        gameMotion = wiimoteMotionReceiver.motion;
+        transform.Translate (gameMotion * Time.deltaTime * moveSpeed, Space.World);
+        transform.position = Vector3.SmoothDamp (transform.position, target.position, ref velocity, smoothTime);
+    }
+}
--- JN2018_04/Assets/Scripts/WiimoteTest.cs
+++ JN2018_04/Assets/Scripts/WiimoteTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using WiimoteApi;
@@ -7,6 +8,10 @@ public class WiimoteTest : MonoBehaviour {
 
     Wiimote remote;
     public Vector3 motion;
+    public Vector3 offset;
+    float lastFramex;
+    float lastFramey;
+    float lastFramez;
 
     void Start () {
         InitWiimotes ();
@@ -24,15 +29,21 @@ public class WiimoteTest : MonoBehaviour {
         }
         remote.SendPlayerLED (true, false, false, false); // LEDs will look like this: [* - - *]
         remote.SendDataReportMode (InputDataType.REPORT_BUTTONS_ACCEL);
-        remote.RumbleOn = true; // Enabled Rumble
-        remote.SendStatusInfoRequest (); // Requests Status Report, encodes Rumble into input report
+        //   remote.RumbleOn = true; // Enabled Rumble
+        //  remote.SendStatusInfoRequest (); // Requests Status Report, encodes Rumble into input report
+        remote.Accel.CalibrateAccel (WiimoteApi.AccelCalibrationStep.A_BUTTON_UP);
+
+        //   float[] accelerationOffsetArray = remote.Accel.GetCalibratedAccelData ();
+        //   offset = new Vector3 (accelerationOffsetArray[1], accelerationOffsetArray[0], accelerationOffsetArray[2]);
+
         Invoke ("StopRumble", 1f);
 
     }
 
     void StopRumble () {
-        remote.RumbleOn = false; // Disabled Rumble
-        remote.SendStatusInfoRequest (); // Requests Status Report, encodes Rumble into input report
+        //    remote.RumbleOn = false; // Disabled Rumble
+        //    remote.SendStatusInfoRequest ();
+        // Requests Status Report, encodes Rumble into input report
     }
     void FinishedWithWiimotes () {
         foreach (Wiimote remote in WiimoteManager.Wiimotes) {
@@ -42,12 +53,45 @@ public class WiimoteTest : MonoBehaviour {
 
     void Update () { // called once per frame (for example)
 
+        if (Input.GetKeyDown ("a")) {
+            remote.Accel.CalibrateAccel (WiimoteApi.AccelCalibrationStep.A_BUTTON_UP);
+        }
+        if (Input.GetKeyDown ("b")) {
+            remote.Accel.CalibrateAccel (WiimoteApi.AccelCalibrationStep.EXPANSION_UP);
+        }
+        if (Input.GetKeyDown ("c")) {
+            remote.Accel.CalibrateAccel (WiimoteApi.AccelCalibrationStep.LEFT_SIDE_UP);
+        }
+
         int ret;
         do {
 
             ret = remote.ReadWiimoteData ();
             float[] accelerationDataArray = remote.Accel.GetCalibratedAccelData ();
-            motion = new Vector3 (accelerationDataArray[1], accelerationDataArray[0], accelerationDataArray[2]);
+
+            /*          float y = (float) Math.Round ((double) accelerationDataArray[0], 2);
+                     float x = (float) Math.Round ((double) accelerationDataArray[1], 2);
+                     float z = (float) Math.Round ((double) accelerationDataArray[2], 2);
+
+                     if (y == lastFramey) {
+                         y = 0f;
+                     }
+                     if (x == lastFramex) {
+                         x = 0f;
+                     }
+                     if (z == lastFramez) {
+                         z = 0f;
+                     }
+                     //    Vector3 temporaryMotion = new Vector3 (accelerationDataArray[1], accelerationDataArray[2], accelerationDataArray[0]);
+                     motion = new Vector3 (x, y, z);
+
+                     lastFramex = x;
+                     lastFramey = y;
+                     lastFramez = z;*/
+
+            Vector3 motionCurrent = new Vector3 ((float) Math.Round ((double) accelerationDataArray[0], 2) * -1f, (float) Math.Round ((double) accelerationDataArray[2], 2), (float) Math.Round ((double) accelerationDataArray[1], 2) * -1f);
+            motion = motionCurrent + offset;
+
         } while (ret > 0); // ReadWiimoteData() returns 0 when nothing is left to read.  So by doing this we continue to
 
     }
