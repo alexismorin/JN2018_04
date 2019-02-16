bf8d5f1347d4629a782d2ceb7e179f6987aef7dc
Alexis <alexis.morin.mail@gmail.com>
2019-02-16 00:09:28 -0500

    added amplify and

---
+++ JN2018_04/Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour {
+
+    GameObject cells;
+
+    void Start () {
+
+    }
+
+    void Update () {
+
+    }
+}
