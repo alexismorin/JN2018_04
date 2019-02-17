f79372d83b8ccfdc021c58794ba679563f645ee4
Alexis <alexis.morin.mail@gmail.com>
2019-02-17 10:59:59 -0500

    map and fixes

--- JN2018_04/Assets/Scripts/Editor/ScrambleZones.cs
+++ JN2018_04/Assets/Scripts/Editor/ScrambleZones.cs
@@ -7,35 +7,35 @@ public class ScrambleZones : MonoBehaviour {
 
     [MenuItem ("Tools/Stock Save")]
     private static void StockSave () {
-        PlayerPrefs.SetString ("guardsCaseCode", "0-10-20-30-");
+        PlayerPrefs.SetString ("guardsCaseCode", "0-10-20-30-40-50-12-25-64-25-63-74-");
     }
+    /*
+        [MenuItem ("Tools/Scramble Zones")]
+        private static void NewMenuOption () {
+            GameObject[] zones = GameObject.FindGameObjectsWithTag ("zone");
+            int[] originalZoneOrder = new int[zones.Length];
 
-    [MenuItem ("Tools/Scramble Zones")]
-    private static void NewMenuOption () {
-        GameObject[] zones = GameObject.FindGameObjectsWithTag ("zone");
-        int[] originalZoneOrder = new int[zones.Length];
+            for (int i = 0; i < originalZoneOrder.Length; i++) {
+                originalZoneOrder[i] = i;
+                print (originalZoneOrder[i]);
 
-        for (int i = 0; i < originalZoneOrder.Length; i++) {
-            originalZoneOrder[i] = i;
-            print (originalZoneOrder[i]);
+            }
 
-        }
+            int[] newZoneOrder = originalZoneOrder;
 
-        int[] newZoneOrder = originalZoneOrder;
+            for (int t = 0; t < newZoneOrder.Length; t++) {
+                int tmp = newZoneOrder[t];
+                int r = Random.Range (t, newZoneOrder.Length);
+                newZoneOrder[t] = newZoneOrder[r];
+                newZoneOrder[r] = tmp;
+            }
 
-        for (int t = 0; t < newZoneOrder.Length; t++) {
-            int tmp = newZoneOrder[t];
-            int r = Random.Range (t, newZoneOrder.Length);
-            newZoneOrder[t] = newZoneOrder[r];
-            newZoneOrder[r] = tmp;
-        }
+            for (int z = 0; z < zones.Length; z++) {
+                zones[z].GetComponent<Zone> ().zoneID = newZoneOrder[z];
+                zones[z].name = "Zone " + newZoneOrder[z].ToString ();
+                print (newZoneOrder[z]);
+            }
 
-        for (int z = 0; z < zones.Length; z++) {
-            zones[z].GetComponent<Zone> ().zoneID = newZoneOrder[z];
-            zones[z].name = "Zone " + newZoneOrder[z].ToString ();
-            print (newZoneOrder[z]);
-        }
-
-    }
+        } */
 
 }
